e0f345760b156e95c231160776a0a2e6276f2592
katiedaly <dalyk@hawaii.edu>
2021-01-19 12:28:36 -1000

    SP #1
    Class 3, Tues. Jan 19

--- TestPlayground.playground/Contents.swift
+++ TestPlayground.playground/Contents.swift
@@ -5,15 +5,15 @@ var str = "Hello, playground"
 //declare a variable, name it myFirstVariable, and type it as a String
 var myFirstVariable: String
 
-//String is a data type that is represented by a series of characters. the string must be surrounded by ""
+//String is a data type that is represented by a series of characters. The string must be surrounded by ""
 
 //assign myFirstVariable to a string
-myFirstVariable = "I've created my first string"
+myFirstVariable = "I've created my first string "
 
 //String Concatenation - combinging strings together
 
 //declare and assign a second variable all in one line
-var mySecondVariable = "and look I combined 2 strings together"
+var mySecondVariable = "and look I combined 2 strings together."
 
 //create a third variable to hold the string concatenation
 
@@ -21,3 +21,15 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+//Part 8, create three variables and assign each of them a string.
+var myFourthVariable = "Here is another string. "
+
+var myFifthVariable = "I think I am getting the hang of this. "
+
+var mySixthVariable = "Hopefully I did this correctly!"
+
+//String concatenation
+var mySeventhVariable = myFourthVariable + myFifthVariable + mySixthVariable
+
+//Print fourth variable
+print(mySeventhVariable)
