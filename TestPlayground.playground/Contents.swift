0e6d812dd78f05be062c505f3746e54e956a0dfe
katiedaly <dalyk@hawaii.edu>
2021-01-21 10:26:25 -1000

    Assignment #2
    Thurs, 1-21

--- TestPlayground.playground/Contents.swift
+++ TestPlayground.playground/Contents.swift
@@ -2,34 +2,59 @@ import UIKit
 
 var str = "Hello, playground"
 
-//declare a variable, name it myFirstVariable, and type it as a String
+// Declare a variable, name it myFirstVariable, and type it as a String
 var myFirstVariable: String
 
-//String is a data type that is represented by a series of characters. The string must be surrounded by ""
+// String is a data type that is represented by a series of characters. The string must be surrounded by ""
 
-//assign myFirstVariable to a string
+// Assign myFirstVariable to a string
 myFirstVariable = "I've created my first string "
 
-//String Concatenation - combinging strings together
+// String Concatenation - combinging strings together
 
-//declare and assign a second variable all in one line
+// Declare and assign a second variable all in one line
 var mySecondVariable = "and look I combined 2 strings together."
 
-//create a third variable to hold the string concatenation
-
+// Create a third variable to hold the string concatenation
 var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
-//Part 8, create three variables and assign each of them a string.
+// Part 8, create three variables and assign each of them a string.
 var myFourthVariable = "Here is another string. "
 
 var myFifthVariable = "I think I am getting the hang of this. "
 
 var mySixthVariable = "Hopefully I did this correctly!"
 
-//String concatenation
+// String concatenation
 var mySeventhVariable = myFourthVariable + myFifthVariable + mySixthVariable
 
-//Print fourth variable
+// Print fourth variable
 print(mySeventhVariable)
+
+
+// Assignment #2: Introduction to True and False
+// Class 4 | Thursday | 1.21.21
+
+var appState = false
+
+// print() - print function prints into the console. All functions have a name and are followed by parenthesis ()
+print(appState)
+
+// declare and assign two variables of type int
+var numberOne = 5
+var numberTwo = 7
+
+// create a statement that compares the two variables and assign the evaluation of the statement to a third variable
+var comparison = numberOne > numberTwo
+
+print(comparison)
+
+// Part 7: Create two different variables and assign those variables to two different strings
+var myEighthVariable = "Aloha!"
+var myNinthVariable = "Aloha!"
+var myComparison = myEighthVariable == myNinthVariable
+
+print(myComparison)
+
